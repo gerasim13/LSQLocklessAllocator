12bfc31f9a65b9bf9ca1d302f0c2c5024a951bc2
Павел Литвиненко <gerasim13@gmail.com>
2014-07-29 05:00:39 +0400

    Added LSQCONTRUCTOR and LSQDESCTRUCTOR keywords.

--- LSQAllocator.h
+++ LSQAllocator.h
@@ -26,24 +26,27 @@ CF_EXPORT CFAllocatorRef kLSQLocklessAllocator;
 
 //________________________________________________________________________________________
 
-#pragma mark - Functions
-
-CF_EXPORT void                   LSQAllocatorSetCurrentAllocator(CFAllocatorRef); // Allocator setter
-CF_EXPORT struct task_basic_info LSQAllocatorGetMemoryInfo();                     // Get memory info
-
-CF_EXPORT void * LSQMalloc (size_t size);
-CF_EXPORT void * LSQCalloc (size_t no, size_t size);
-CF_EXPORT void * LSQRealloc(void *mem, size_t size);
-CF_EXPORT void   LSQFree   (void *mem);
-
-//________________________________________________________________________________________
-
 #pragma mark - Macros
 
 #define LSQAllocatorAllocSize(size)    CFAllocatorAllocate(kLSQLocklessAllocator, size, 0);
 #define LSQAllocatorAllocType(type)    LSQAllocatorAllocSize(sizeof(type));
 #define LSQAllocatorDealloc(ptr)       CFAllocatorDeallocate(kLSQLocklessAllocator, ptr); ptr = NULL;
 #define LSQAllocatorRealloc(ptr, size) CFAllocatorReallocate(kLSQLocklessAllocator, ptr, size, 0);
+
+#define LSQCONSTRUCTOR __attribute((ownership_returns(malloc)))
+#define LSQDESTRUCTOR  __attribute((ownership_takes(malloc, 1)))
+
+//________________________________________________________________________________________
+
+#pragma mark - Functions
+
+CF_EXPORT void                   LSQAllocatorSetCurrentAllocator(CFAllocatorRef); // Allocator setter
+CF_EXPORT struct task_basic_info LSQAllocatorGetMemoryInfo();                     // Get memory info
+
+LSQCONSTRUCTOR CF_EXPORT void * LSQMalloc (size_t size);
+LSQCONSTRUCTOR CF_EXPORT void * LSQCalloc (size_t no, size_t size);
+LSQDESTRUCTOR  CF_EXPORT void   LSQFree   (void *mem);
+               CF_EXPORT void * LSQRealloc(void *mem, size_t size);
 
 //________________________________________________________________________________________
 
